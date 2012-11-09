0ee403c09ad0dda6bbfe2877be9d21df06db3f71
Radim Marek <radim@laststation.net>
2012-11-09 10:38:26 +0100

    Exclude service users from stats

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HomeController < Admin::BaseController
 	def index
-		@users = User.all.sort(created_at:-1).limit(10)
+		@users = User.where(service: false).sort(created_at:-1).limit(10)
 	end
 end
